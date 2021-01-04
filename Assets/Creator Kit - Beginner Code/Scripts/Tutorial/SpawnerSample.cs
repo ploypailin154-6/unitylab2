1d5752d782331c85a6ec8382948112172b781c3f
ploypailin154-6 <ploypailin_ppp@kkumail.com>
2021-01-04 14:32:43 +0700

    Re-declare the variables in function

--- Assets/Creator Kit - Beginner Code/Scripts/Tutorial/SpawnerSample.cs
+++ Assets/Creator Kit - Beginner Code/Scripts/Tutorial/SpawnerSample.cs
@@ -39,8 +39,10 @@ public class SpawnerSample : MonoBehaviour
     }
     void SpawnPotion(int angle)
     {
+        int radius = 5;
+
         Vector3 direction = Quaternion.Euler(0, angle, 0) * Vector3.right;
-        spawnPosition = transform.position + direction * radius;
+        Vector3 spawnPosition = transform.position + direction * radius;
         Instantiate(ObjectToSpawn, spawnPosition, Quaternion.identity);
     }
 }
