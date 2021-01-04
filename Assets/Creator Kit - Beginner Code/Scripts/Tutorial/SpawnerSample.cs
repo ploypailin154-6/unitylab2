6e6af2e3e6799e9151dcc102f3aae85355bc7aaa
ploypailin154-6 <ploypailin_ppp@kkumail.com>
2021-01-04 14:35:08 +0700

    Replace the spawning instructions with call functions

--- Assets/Creator Kit - Beginner Code/Scripts/Tutorial/SpawnerSample.cs
+++ Assets/Creator Kit - Beginner Code/Scripts/Tutorial/SpawnerSample.cs
@@ -9,27 +9,10 @@ public class SpawnerSample : MonoBehaviour
     {
         int radius = 5;
         int angle = 15;
-        Vector3 spawnPosition = transform.position;
-
-        Vector3 direction = Quaternion.Euler(0, angle, 0) * Vector3.right;
-        spawnPosition = transform.position + direction * radius;
-        Instantiate(ObjectToSpawn, spawnPosition, Quaternion.identity);
-
-        angle = 55;
-        direction = Quaternion.Euler(0, angle, 0) * Vector3.right;
-        spawnPosition = transform.position + direction * radius;
-        Instantiate(ObjectToSpawn, spawnPosition, Quaternion.identity);
-
-        angle = 95;
-        direction = Quaternion.Euler(0, angle, 0) * Vector3.right;
-        spawnPosition = transform.position + direction * radius;
-        Instantiate(ObjectToSpawn, spawnPosition, Quaternion.identity);
-
-        // Fourth potion created at spawn
-        angle = 135;
-        direction = Quaternion.Euler(0, angle, 0) * Vector3.right;
-        spawnPosition = transform.position + direction * radius;
-        Instantiate(ObjectToSpawn, spawnPosition, Quaternion.identity);
+        SpawnPotion(0);
+        SpawnPotion(45);
+        SpawnPotion(90);
+        SpawnPotion(135);
     }
     void AddingNumbers(float num1, float num2)
     {
